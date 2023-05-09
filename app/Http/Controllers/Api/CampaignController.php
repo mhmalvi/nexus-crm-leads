7ab393e034c30ab08ea6cd408ee9582e948e5d17
Habib <habib@quadque.digital>
2023-05-09 18:21:03 +0600

    latest

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -45,20 +45,32 @@ class CampaignController extends Controller
 
     public function campaign_wise_lead_percentage(Request $request)
     {
-        // dd($request->client_id);
         try {
             if ($request->client_id) {
                 $total_lead = LeadDetails::select('lead_id')->where('client_id', $request->client_id)->distinct()->count();
-                $campaigns = LeadDetails::select('lead_id', 'campaign_id')->distinct('campaign_id')->where('client_id', $request->client_id)->groupBy('campaign_id')->get();
-                // dd(json_encode($campaigns));
+                $campaigns = LeadDetails::select('campaign_id')->distinct('campaign_id')->where('client_id', $request->client_id)->get();
+                foreach ($campaigns as $campaign) {
+                    // dd(json_encode($campaign->campaign_id));
+                    $campaign_id[] = LeadDetails::select(DB::raw('count(lead_id) as lead'), DB::raw('campaign_id'))->where('campaign_id', $campaign->campaign_id)->get();
+                }
+                // foreach($campaign_id as $id){
+                // $data = collect($campaign_id;
+                // }
+                // dd(json_encode($campaign_id));
+                // for($i=0;$i<count($campaign_id);$i++){
+                //     dd(explode(',',$campaign_id[$i][0]));
+                // }
+                // dd(explode(',',$data));
+
+                dd(json_encode($campaign_id));
                 // $percentage = ($campaigns->count()/$total_lead)*100;
                 // dd(round($percentage,2));
-                if ($campaigns) {
+                if ($campaign_id) {
                     return response()->json([
                         'message' => 'success',
                         'status' => 200,
                         //   'data'=>round($percentage,2)
-                        'data' => $campaigns
+                        'data' => collect($campaign_id)->flatten(1)->toArray()
                     ]);
                 } else {
                     return response()->json([
