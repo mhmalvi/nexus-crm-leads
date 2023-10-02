d93fa0805457bb29d45b9d219b8716267e08add9
TanjibQuad <tanjib@quadque.tech>
2023-10-02 08:55:02 +0600

    lead amount history restored from backup

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -69,6 +69,38 @@ class LeadController extends Controller
         }
     }
 
+    public function leadAddAmount(Request $request)
+    {
+
+        if (!isset($request->lead_id) || !isset($request->amount)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Lead id and Lead Amount required'
+            ], 406);
+        }
+
+
+        try {
+
+            LeadAmountHistory::updateOrcreate([
+                'lead_id' => $request->lead_id,
+                'amount' => $request->amount
+            ])->toArray();
+            $leadAmountHistory = LeadAmountHistory::where('lead_id', '=', $request->lead_id)->get()->toArray();
+            return response()->json([
+                'status' => true,
+                'message' => 'Lead amount added successfully',
+                'data'   => $leadAmountHistory
+
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
     public function leadAddCallHistory(Request $request)
     {
 
