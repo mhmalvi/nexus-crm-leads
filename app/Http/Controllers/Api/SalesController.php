9b9c456f22535e87bdaa373aa2c35d66f885e9b3
TanjibQuad <tanjib@quadque.tech>
2023-12-20 16:44:49 +0600

    latest

--- app/Http/Controllers/Api/SalesController.php
+++ app/Http/Controllers/Api/SalesController.php
@@ -53,49 +53,49 @@ class SalesController extends Controller
 
     public function assigned_leads(Request $request, $id)
     {
-        // if($request->bearerToken()){
-        //     $userApi = env('USER_SERVICE_API', '');
-        $flag = Http::withToken($request->bearerToken())->post($userApi . '/check-if-token-exists');
-        //     $flag_receive = $flag['data'];
-        //     if($flag_receive == 1){
-        $leads = DB::table('lead_details')->join('courses_info', 'lead_details.course_id', '=', 'courses_info.id')->select('lead_details.lead_id as lead_id', 'lead_details.full_name as full_name', 'courses_info.course_title as course', 'lead_details.sales_user_id as sales_user_id',)->where('lead_details.sales_user_id', $id)->get()->toArray();
-        for ($i = 0; $i < count($leads); $i++) {
-            $count = DB::table('counts')->select('call_count')->where('lead_id', $leads[$i]->lead_id)->first();
-            $leads[$i]->count = $count;
-        }
-        for ($j = 0; $j < count($leads); $j++) {
-            $last_count = DB::table('lead_call_history')->where('lead_id', $leads[$j]->lead_id)->max('call_start_time');
-            $leads[$j]->last_call = $last_count;
-        }
-        if ($leads) {
-            return response()->json([
-                'message' => 'success',
-                'status' => 200,
-                'data' => $leads
-            ], 200);
+        if ($request->bearerToken()) {
+            $userApi = env('USER_SERVICE_API', '');
+            $flag = Http::withToken($request->bearerToken())->post($userApi . '/check-if-token-exists');
+            $flag_receive = $flag['data'];
+            if ($flag_receive == 1) {
+                $leads = DB::table('lead_details')->join('courses_info', 'lead_details.course_id', '=', 'courses_info.id')->select('lead_details.lead_id as lead_id', 'lead_details.full_name as full_name', 'courses_info.course_title as course', 'lead_details.sales_user_id as sales_user_id',)->where('lead_details.sales_user_id', $id)->get()->toArray();
+                for ($i = 0; $i < count($leads); $i++) {
+                    $count = DB::table('counts')->select('call_count')->where('lead_id', $leads[$i]->lead_id)->first();
+                    $leads[$i]->count = $count;
+                }
+                for ($j = 0; $j < count($leads); $j++) {
+                    $last_count = DB::table('lead_call_history')->where('lead_id', $leads[$j]->lead_id)->max('call_start_time');
+                    $leads[$j]->last_call = $last_count;
+                }
+                if ($leads) {
+                    return response()->json([
+                        'message' => 'success',
+                        'status' => 200,
+                        'data' => $leads
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'message' => 'No leads found',
+                        'status' => 404
+                    ], 404);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'Unauthenticated',
+                    'status' => 401
+                ], 401);
+            }
         } else {
             return response()->json([
-                'message' => 'No leads found',
-                'status' => 404
-            ], 404);
+                'message' => 'Unauthenticated',
+                'status' => 401
+            ], 401);
         }
-        //     }else{
-        //         return response()->json([
-        //                 'message' => 'Unauthenticated',
-        //                 'status' => 401
-        //             ], 401);
-        //     }
-        // }else{
-        //     return response()->json([
-        //                 'message' => 'Unauthenticated',
-        //                 'status' => 401
-        //             ], 401);
-        // }
     }
 
     public function unassigned_leads(Request $request, $id)
     {
-        // $userApi = env('USER_SERVICE_API', '');
+        $userApi = env('USER_SERVICE_API', '');
         $flag = Http::withToken($request->bearerToken())->post($userApi . '/check-if-token-exists');
         // $flag_receive = $flag['data'];
         // if($flag_receive == 1){
