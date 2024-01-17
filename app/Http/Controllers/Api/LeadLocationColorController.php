1bfe87e0369c70d9df8c1c640b90db8e29f8b55e
TanjibQuad <tanjib@quadque.tech>
2024-01-17 11:45:51 +0600

    lead service api integrated

---
+++ app/Http/Controllers/Api/LeadLocationColorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeadLocationColorController extends Controller
+{
+    public function add_color(Request $request){
+        
+    }
+}
